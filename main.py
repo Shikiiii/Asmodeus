dae0f5278d08fe18b3449a342d4b2309adc49b69
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2019-11-03 10:50:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3321,9 +3321,9 @@ async def restart(ctx):
 
 
 @bot.command()
-async def r(ctx, id: int):
+async def r(ctx, id: int, *, content: str):
     msg = await bot.fetch_message(id)
-    embed = discord.Embed(description="> {}\n\n{}".format(msg.content, ctx.message.content))
+    embed = discord.Embed(description="> {}\n\n{}".format(msg.content, content))
     msg3 = await ctx.send(f"**{ctx.message.author.name}** replied to **{msg.author.name}**:", embed=embed)
     msg2 = discord.Embed(description="Ay, **{}** replied to your message in **{}**. \n[Jump to the reply.](https://discordapp.com/channels/{}/{}/{})".format(ctx.message.author.name, ctx.message.author.guild.name, msg3.author.id, ctx.message.author.guild.id, msg3.id))
     await msg.author.send(embed=msg2)
