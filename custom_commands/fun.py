e0a7b97daceef6e346aa6978b9d4b94f2052a0da
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2019-11-22 16:40:43 +0200

    Create fun.py

---
+++ custom_commands/fun.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+
+import random
+import sys
+import traceback
+import asyncio
+import datetime
+import json
+
+from common_vars import *
+
+# Commands in this file:
+# blush, pat, kiss, hug, cuddle, slap, howgay, 
+# howlesbian, thotrate, 8ball,  rate, roast, penis, ship, coinflip
