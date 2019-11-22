bd5c9dd280bb999639ccbff06bba85c05edb94ee
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2019-11-22 17:49:37 +0200

    Update admin.py

--- custom_commands/admin.py
+++ custom_commands/admin.py
@@ -1,1 +1,14 @@
+import discord
+from discord.ext import commands
 
+import random
+import sys
+import traceback
+import asyncio
+import datetime
+import json
+
+from common_vars import *
+
+# Commands in this file:
+# lockdown, role
