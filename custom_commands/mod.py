72180d9a30dd105669b203672cbcdcb6270edf81
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2019-11-22 17:48:42 +0200

    Update mod.py

--- custom_commands/mod.py
+++ custom_commands/mod.py
@@ -1,1 +1,15 @@
+import discord
+from discord.ext import commands
 
+import random
+import sys
+import traceback
+import asyncio
+import datetime
+import json
+
+from common_vars import *
+
+# Commands in this file:
+# ban, unban, banid, kick, mute, unmute,
+# purge, clean, bots
