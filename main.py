8ed90b05b531872b5c0aa6d16ee691dc679d8c76
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2019-11-22 10:16:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -345,6 +345,8 @@ async def on_message(message: Message):
         await message.channel.send("dm <@567799351368482826> for a Daddy :wink:")
     elif message.content == "madz":
         await message.channel.send("shrek is love, shrek is life")
+    elif message.content == "meg":
+        await message.channel.send("wooden hoe")
 
     await bot.process_commands(message)
 
