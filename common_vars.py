963d2815dee1afae1fdd4e24fa5573b1bcb083ad
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2019-11-23 19:12:43 +0200

    Update common_vars.py

--- common_vars.py
+++ common_vars.py
@@ -26,6 +26,9 @@ msgsCounterr = 0
 msgsCounterrr = 0
 allTimeMessages = 0
 
+storage = bot.get_guild(646432280365236235)
+storagePrefix = bot.get_channel(646432846961049601)
+
 async def convert_color_menu(role_number: str):
     if role_number == "1":
         return "Light Red"
