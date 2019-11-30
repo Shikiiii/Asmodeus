49c0e408698ec7a0f7302691cfe22385492e5752
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2019-11-30 20:02:13 +0200

    Update economy.py

--- custom_commands/economy.py
+++ custom_commands/economy.py
@@ -1,1 +1,34 @@
+import discord
+from discord.ext import commands
 
+import random
+import sys
+import traceback
+import asyncio
+import datetime
+import json
+
+from datetime import datetime
+
+from common_vars import *
+
+# Import for cooldowns.
+from discord.ext.commands.cooldowns import BucketType
+
+@bot.command()
+@commands.cooldown(1, 86400, BucketType.user)
+async def daily(ctx):
+    for key, value in balances.items():
+        if int(key) == ctx.message.author.id:
+            bal = int(value)
+            new_bal = bal+500
+            balances[int(key)] == new_bal
+            embed = discord.Embed(description="You claimed your daily $500!", timestamp=datetime.utcnow(), color=0x000000)
+            embed.set_author(name="{}".format(ctx.message.author.name), icon_url=ctx.message.author.avatar_url)
+            await ctx.send(embed=embed)
+            return
+    balances[ctx.message.author.id] == 500
+    embed = discord.Embed(description="You claimed your daily $500!", timestamp=datetime.utcnow(), color=0x000000)
+    embed.set_author(name="{}".format(ctx.message.author.name), icon_url=ctx.message.author.avatar_url)
+    await ctx.send(embed=embed)
+    return
