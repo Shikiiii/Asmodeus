896098841bd2410b04eaffc8d04312919c87e9e8
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2019-12-04 14:58:29 +0200

    Update events.py

--- custom_commands/events.py
+++ custom_commands/events.py
@@ -33,8 +33,15 @@ async def on_ready():
     storageMM = storage.get_channel(648951574319726592)
     storageP= storage.get_channel(648951548809838628)
     storageUP = storage.get_channel(646432281287852057)
+    storageMC = storage.get_channel(651763336425373707)
     
     # Here, we get the prefixes, so we don't have to scan the channel everytime a command is ran. - Shiki
+    async for message in storageMC.history():
+        x = message.content.split("|")
+        userID = x[0]
+        ign = x[2]
+        mcIGNs[int(userID)] = ign
+        mcIGNsToDelete[int(userID)] = message.id
     async for message in storageUP.history():
         x = message.content.split("|")
         userID = x[0]
