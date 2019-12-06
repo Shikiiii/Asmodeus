dc7bb725db04a1c0c6af4349c8dae2e2ad99f5d1
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2019-12-06 17:48:10 +0200

    Update events.py

--- custom_commands/events.py
+++ custom_commands/events.py
@@ -322,7 +322,7 @@ async def on_message(message: Message):
     if (message.content == "!welcome" and (
             message.author.id == 237938976999079948 or message.author.id == 495680416422821888)):
         embed1 = discord.Embed(title="**༚ ✧˳⁺ __Welcome to Ａｓｍｏｄｅｕｓ!__  ⁺˳✧ ༚**",
-                               description="- We are so glad to have you join our server! By joining this server you agreed to our rules. \r\n\r\n - We have over 100+ roles, channels and some bots to play different games and much more! \r\n\r\n - Our channels are not aggressively moderated so feel free to join any conversation you like.",
+                               description="We're so glad to have you here! By staying in this server, you agree to our __rules__. We have over 100 bots, channels and roles to play around with! Make sure to stick around for more cool stuff. We are a friendly community, we accept everyone regardless of your gender, age, race or etc.",
                                color=0xC5FCFC)
         embed2 = discord.Embed(color=0xC5FCFC)
         embed2.set_image(url="https://cdn.discordapp.com/attachments/635581513228091462/637970310095831061/Untitled-1.png")
@@ -331,15 +331,16 @@ async def on_message(message: Message):
     elif (message.content == "!rules" and (
             message.author.id == 237938976999079948 or message.author.id == 495680416422821888)):
         embed1 = discord.Embed(title="**༚ ✧˳⁺ __Server rules__ ⁺˳✧ ༚**",
-                               description="__``1``__ • Make sure to follow the [Discord TOS](https://discordapp.com/terms) and the [Community Guidelines](https://discordapp.com/guidelines). \r\n\r\n __``2``__ • Keep content in the appropriate channels. This includes only posting NSFW content in NSFW marked channels. \r\n\r\n __``3``__ • No Doxing / Do not leak someone's IP address / Do not share personal information of anyone. \r\n\r\n __``4``__ • Any type of serious harassment will result in a ban. If you're not sure about whether something you've done is harassment, check out the [guidelines](https://discordapp.com/guidelines). \r\n\r\n __``5``__ • No advertising (Including PM advertisement). \r\n\r\n __``6``__ • Do NOT tell people to kill themselves (even if it's a joke). \r\n\r\n __``7``__ • Do not impersonate other users. Impersonating our staff team or discord staff will result in a permanent ban. \r\n\r\n __``8``__ • Remember that this server is English only! Try to avoid using any other languages so everyone can understand each other and have fun. \r\n\r\n __``9``__ • Respect all staff and follow their instruction(s). \r\n\r\n __``10``__ • Do NOT set people up against each other. \r\n\r\n __``11``__ • Respect people's wishes considering revealing their age, face reveal etc. \r\n\r\n __``12``__ • Asking for nudes or for other information is ABSOLUTELY not accepted here and will result in a ban. \r\n\r\n __``13``__ • The N-Word has been banned as of 28.11.19 because the large amount of people that feel uncomfortable with it. If enough people convince me to, I will unban it. Thanks for understanding. \r\n\r\n **- Please contact <@237938976999079948> if you have any issues with our staff team and/or the rules posted above.** \r\n\r\n **- Thank you for joining Ａｓｍｏｄｅｕｓ! We hope you have a great stay here!**",
+                               description="``1`` - **Make sure to follow the [Discord TOS](https://discordapp.com/terms) and the [Community Guidelines](https://discordapp.com/guidelines)**. \n\n``2`` - This is a **friendly community**. Any toxicity, hate or racism is banned.\n\n``3`` - Hard 'R' is banned unless you're black.\n\n``4`` - Playing a mf hacker is a really, really dumb move. Linking someone's personal information (doxing them) would get you instantly banned.\n\n``5`` - Advertising is completely banned (including DM/PM advertising). If someone's DMing you ads of any sort, let staff know, they'll handle it.\n\n``6`` - Respect all members and staff, their decision(s) and wishes.\n\nOther than that, please **use common sense** while you are typing in any of the text channels. This means no spamming, raiding, insulting, etc.\n\nFor any problems: <@237938976999079948> | If you've found any loopholes in the rules, DM <@237938976999079948>.",
+                               timestamp=datetime.utcnow(),
                                color=0xC5FCFC)
-        embed2 = discord.Embed(color=0xC5FCFC)
-        embed2.set_image(url="https://media.giphy.com/media/kD0G3PwfsUhUzQu3QQ/giphy.gif")
-        embed3 = discord.Embed(title="**༚ ✧˳⁺ __Voice Chat rules__ ⁺˳✧ ༚**",
-                               description="__``1``__ • Don't ear rape other people with music or with your mic. \r\n\r\n __``2``__ • Do not spam music, let other people play their song. \r\n\r\n __``3``__ • Do not stop the music if there are still others in the voice channel. \r\n\r\n __``4``__ • Do not mic spam, yell or disturb others. \r\n\r\n __``5``__ • Do NOT be toxic or be racist.",
-                               color=0xC5FCFC)
-        await message.channel.send(embed=embed2)
-        await message.channel.send(embed=embed3)
+        #embed2 = discord.Embed(color=0xC5FCFC)
+        #embed2.set_image(url="https://media.giphy.com/media/kD0G3PwfsUhUzQu3QQ/giphy.gif")
+        #embed3 = discord.Embed(title="**༚ ✧˳⁺ __Voice Chat rules__ ⁺˳✧ ༚**",
+        #                       description="__``1``__ • Don't ear rape other people with music or with your mic. \r\n\r\n __``2``__ • Do not spam music, let other people play their song. \r\n\r\n __``3``__ • Do not stop the music if there are still others in the voice channel. \r\n\r\n __``4``__ • Do not mic spam, yell or disturb others. \r\n\r\n __``5``__ • Do NOT be toxic or be racist.",
+         #                      color=0xC5FCFC)
+        #await message.channel.send(embed=embed2)
+        #await message.channel.send(embed=embed3)
         await message.channel.send(embed=embed1)
     elif (message.content == "!faq" and (
             message.author.id == 237938976999079948 or message.author.id == 495680416422821888)):
