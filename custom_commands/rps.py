ea85ef79f50f12df9fb9a52442e9895f0a4723ca
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2020-02-14 21:28:17 +0200

    Create rps.py

---
+++ custom_commands/rps.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+
+import random
+import sys
+import traceback
+import asyncio
+import datetime
+import json
+
+from datetime import datetime
+
+from common_vars import *
+
+# plan: list w people already in game, 
+
+
+
+
+
+
+
+async def converter(ctx, one: int, two: int):
+  
