b2d36bb09f914c4326e05bbed5e3f20d253ac069
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2020-03-03 16:43:50 +0200

    Update beta.py

--- custom_commands/beta.py
+++ custom_commands/beta.py
@@ -17,8 +17,8 @@ from datetime import datetime
 
 @bot.command()
 async def fixrolemenupings(ctx):
-      chan = bot.get_channel(id)
-      msg = await chan.fetch_message(id)
+      chan = bot.get_channel(680532444553674773)
+      msg = await chan.fetch_message(683687197974724703)
       pings = discord.Embed(description=":star2:«─────── « ⋅ʚ♡ɞ⋅ » ───────»:star2: \n**✧༺༻✧♡༻∞　Pings　∞༺♡✧༺༻✧**\n\n꒰:dizzy:꒱ <@&680813408958021665> \n꒰:sparkles:꒱ <@&680813442747203605> \n꒰:ringed_planet:꒱ <@&680813479333986386> \n꒰:zap:꒱ <@&684408442366328832>\n\n«──────────────────────»", color=0xFA4B88)
       pings.set_image(url="https://cdn.discordapp.com/attachments/680532449989492750/683015182909440111/tenor_6.gif")
       await msg.edit(embed=pings)
